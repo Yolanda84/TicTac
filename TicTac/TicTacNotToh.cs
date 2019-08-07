ad94d508149402cad01b9c3dd604e3c31cb30e98
Yolanda84 <yolandamichaels84@yahoo.com>
2019-08-08 00:22:39 +0300

    validate

--- TicTac/TicTacNotToh.cs
+++ TicTac/TicTacNotToh.cs
@@ -23,56 +23,63 @@ namespace TicTac {
         private void btnOne_Click(object sender, EventArgs e) {
 
             btnOne.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnTwo_Click(object sender, EventArgs e) {
 
             btnTwo.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnThree_Click(object sender, EventArgs e) {
 
             btnThree.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnFour_Click(object sender, EventArgs e) {
 
             btnFour.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnFive_Click(object sender, EventArgs e) {
 
             btnFive.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnSix_Click(object sender, EventArgs e) {
 
             btnSix.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnSeven_Click(object sender, EventArgs e) {
 
             btnSeven.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnEight_Click(object sender, EventArgs e) {
 
             btnEight.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
         private void btnNine_Click(object sender, EventArgs e) {
 
             btnNine.Text = ticTacBtn;
+            ValidateTicTaToh();
         }
 
-        public bool validateTicTaToh() {
+        public void ValidateTicTaToh() {
 
-            if (btnOne.Text == "X" && btnTwo.Text == "X" && btnThree.Text == "X") {
+            if (btnOne.Text == ticTacBtn && btnTwo.Text == ticTacBtn && btnThree.Text == ticTacBtn) {
 
                 MessageBox.Show("You won");
             }
-
-            return true;
         }
 
                         
